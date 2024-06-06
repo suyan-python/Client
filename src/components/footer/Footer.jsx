51032ba133f071093a40e884c7ad2986dcaf6358
Suyan Man Amatya <srkamatya007@gmail.com>
2024-06-06 22:49:01 +0545

    footer section add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import "./Footer.css";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
   return (
     <>
-      <div className="footer-area h-48  bg-slate-400 flex justify-center items-center mt-20 text-white rounded-xl">
-        <div className="text-area">@SuyanManAmatya</div>
-      </div>
+      <footer className="footer">
+        <div className="waves">
+          <div className="wave" id="wave1"></div>
+          <div className="wave" id="wave2"></div>
+          <div className="wave" id="wave3"></div>
+          <div className="wave" id="wave4"></div>
+        </div>
+
+        {/* <ul className="menu">
+          <li className="menu__item">
+            <NavLink className="menu__link" to="/departments/Civil">
+              Civil
+            </NavLink>
+          </li>
+          <li className="menu__item">
+            <NavLink className="menu__link" to="/civil">
+              Computer
+            </NavLink>
+          </li>
+          <li className="menu__item">
+            <NavLink className="menu__link" to="/civil">
+              Architecture
+            </NavLink>
+          </li>
+        </ul> */}
+        <div className="container">
+          <div className="text-area">©All Rights Reserved Inc,2081</div>
+          <div className="follow-us">
+            <div className="title">Contact</div>
+            <div className="social-icons">
+              <div className="social-icon">Facebook</div>
+              <div className="social-icon">Twitter</div>
+              <div className="social-icon">GitHub</div>
+            </div>
+          </div>
+        </div>
+
+        <p>@SuyanManAmatya</p>
+      </footer>
     </>
   );
 }
